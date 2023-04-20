bdd7eb05530f3a89571c3bc79d2f5f660ab5ea85
RoKett17 <rokettish@gmail.com>
2023-04-20 19:28:22 -0400

    def class

---
+++ Final Proj.py
@@ -0,0 +1,8 @@
+class Traits:
+    
+    def __init__(self, name, attack, speed, armor, health):
+        self.name = name
+        self.attack = attack
+        self.speed = speed
+        self.armor = armor
+        self.health = health
