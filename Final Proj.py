aa46ec39a30993036a8c1f39d7176456ee0da6c2
RoKett17 <109094260+RoKett17@users.noreply.github.com>
2023-04-20 20:47:16 -0400

    Delete Final Proj.py

--- Final Proj.py
+++
@@ -1,8 +0,0 @@
-class Traits:
-    
-    def __init__(self, name, attack, speed, armor, health):
-        self.name = name
-        self.attack = attack
-        self.speed = speed
-        self.armor = armor
-        self.health = health
